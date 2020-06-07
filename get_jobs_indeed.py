9c5f6404a0f0e9b6159379de726cc661a73818b2
NavTheRaj <navrajkhanal61@gmail.com>
2020-06-07 15:30:03 +0545

    Resolved comments

--- get_jobs_indeed.py
+++ get_jobs_indeed.py
@@ -21,31 +21,21 @@ results = soup.find('td', id='resultsCol')
 
 job_elems = results.find_all(class_='jobsearch-SerpJobCard')
 
-for job_elem in job_elems:
-    raw_link = job_elem.find('a')['href']
-    pre_raw_link = raw_link.replace("/rc/clk","https://www.indeed.com/viewjob")
-    link = re.sub('&[^>]+&', '&from=serp&', pre_raw_link)
+if job_elems:
+    for job_elem in job_elems:
+        raw_link = job_elem.find('a')['href']
+        pre_raw_link = raw_link.replace("/rc/clk","https://www.indeed.com/viewjob")
+        link = re.sub('&[^>]+&', '&from=serp&', pre_raw_link)
 
-    title_elem = job_elem.find('h2', class_='title')
-    company_elem = job_elem.find('span', class_='company')
-    location_elem = job_elem.find('span', class_='location')
-    print(title_elem.text.strip())
-    print(company_elem.text.strip())
-    print(location_elem.text.strip())
-    print(f"Apply here: {link}\n")
-    print()
-
-#print(results.prettify())
-
-#python_jobs = results.find_all('h2',string=lambda text:job_elem in text.lower())
-
-'''
-if python_jobs:
-    for p_job in python_jobs:
-        link = p_job.find('a')['href']
-        print(p_job.text.strip())
+        title_elem = job_elem.find('h2', class_='title')
+        company_elem = job_elem.find('span', class_='company')
+        location_elem = job_elem.find('span', class_='location')
+        print(title_elem.text.strip())
+        print(company_elem.text.strip())
+        print(location_elem.text.strip())
         print(f"Apply here: {link}\n")
+        print()
+
 else:
     print("Sorry! At the mean time, we have no job for",job_str)
     print("Keep on Searching You will get it!!")
-    '''
